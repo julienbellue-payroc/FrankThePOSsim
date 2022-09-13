0e9218cc3b25e2df2632ad3cff99ce8732f4c4d9
jbellue <julien.bellue@payroc.com>
2022-09-13 09:38:23 +0200

    Make Environment data init only

--- FrankThePOSsim/Environment.cs
+++ FrankThePOSsim/Environment.cs
@@ -4,8 +4,8 @@ namespace FrankThePOSsim;
 
 public class Environment
 {
-    public string? Name { get; set;  }
-    public string? RestUrl { get; set; }
-    public string? SoapUrl { get; set; }
-    public List<Terminal>? Terminals { get; set; }
+    public string? Name { get; init;  }
+    public string? RestUrl { get; init; }
+    public string? SoapUrl { get; init; }
+    public List<Terminal>? Terminals { get; init; }
 }

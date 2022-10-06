2cc0ea3b02809656613b5d610067dc35e8e349d1
jbellue <julien.bellue@payroc.com>
2022-10-06 11:39:55 +0200

    Add unhandled exceptions handler

--- FrankThePOSsim/App.xaml.cs
+++ FrankThePOSsim/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Windows;
@@ -40,9 +41,10 @@ public partial class App
             Current.Shutdown();
         }
 
+        SetupExceptionHandling();
+
         var serviceCollection = new ServiceCollection();
         ConfigureServices(serviceCollection);
-
         ServiceProvider = serviceCollection.BuildServiceProvider();
 
         PreloadResources();
@@ -78,5 +80,42 @@ Create a default one?
     {
         services.AddTransient(typeof(MainWindow));
         services.Configure<Config>(Configuration?.GetSection(nameof(Config)));
+    }
+    
+    private void SetupExceptionHandling()
+    {
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+            HandleUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException", false);
+
+        DispatcherUnhandledException += (_, e) =>
+        {
+            // If we are debugging, let Visual Studio handle the exception and take us to the code that threw it.
+            if (!Debugger.IsAttached)
+            {
+                e.Handled = true;
+                HandleUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException", true);
+            }
+        };
+    }
+
+    private static void HandleUnhandledException(Exception e, string source, bool promptUserForShutdown)
+    {
+
+        var messageBoxTitle = $"Unexpected Error Occurred: {source}";
+        var messageBoxMessage = $"The following exception occurred:\n\n{e}";
+        var messageBoxButtons = MessageBoxButton.OK;
+        var messageBoxIcon = MessageBoxImage.Error;
+
+        if (promptUserForShutdown)
+        {
+            messageBoxMessage += "\n\nNormally Frank would die now. Should we let him die?";
+            messageBoxButtons = MessageBoxButton.YesNo;
+        }
+
+        // Let the user decide if the app should die or not (if applicable).
+        if (MessageBox.Show(messageBoxMessage, messageBoxTitle, messageBoxButtons, messageBoxIcon) == MessageBoxResult.Yes)
+        {
+            Current.Shutdown();
+        }
     }
 }

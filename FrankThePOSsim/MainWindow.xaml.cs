644fd0a0008f2f0536997a0d1d0a12b43fe11748
jbellue <julien.bellue@payroc.com>
2022-12-16 11:36:28 +0100

    Update version number

--- FrankThePOSsim/MainWindow.xaml.cs
+++ FrankThePOSsim/MainWindow.xaml.cs
@@ -44,7 +44,7 @@ public partial class MainWindow
         SetTheme();
 
 
-        Title = "Frank the POSsim 1.2";
+        Title = "Frank the POSsim 1.3";
         ComboBoxEnvironment.DisplayMemberPath = "Name";
         if (_configuration.Environments != null)
             ComboBoxEnvironment.ItemsSource = new EnvironmentObservable(_configuration.Environments);
